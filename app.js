76decbe7605c260466cbdc86d0e07d7af9e3ecf0
nahuelbrizu <nahuelbrizu@outlook.com>
2022-09-06 18:48:40 -0300

    updateCards

--- app.js
+++ app.js
@@ -53,16 +53,17 @@ slider.forEach((slide, index) => {
     slide.addEventListener('mousemove', touchMove)
     slide.addEventListener('mouseleave', touchEnd)
 })
-window.addEventListener('resize', setPositionByIndex)
+window.addEventListener('resize', setPositionByIndex);
 
 window.oncontextmenu = function (event) {
     event.preventDefault();
-    event.stopPropagation()
+    event.stopPropagation();
     return false
 }
 
 function getPositionX(event) {
     return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX
+
 }
 
 function touchStart(index) {
@@ -72,6 +73,7 @@ function touchStart(index) {
         isDragging = true
         animationID = requestAnimationFrame(animation)
         container.classList.add('grabbing')
+        setSliderPosition();
     }
 }
 
@@ -79,6 +81,8 @@ function touchMove(event) {
     if (isDragging) {
         const currentPosition = getPositionX(event)
         currenTranslate = prevTranslate + currentPosition - startPos
+    } else {
+        container.style.touchAction = `translateX(${currenTranslate * 4}px)`
     }
 }
 
@@ -86,11 +90,11 @@ function touchEnd() {
     cancelAnimationFrame(animationID)
     isDragging = false
     const movedBy = currenTranslate - prevTranslate
-    container.classList.add('grab')
 
-    if (movedBy < -100 && currentIndex < slider.length - 1) currentIndex += 1
-    if (movedBy > 100 && currentIndex > 0) currentIndex - 1
+    if (movedBy < (-100) && currentIndex < slider.length - 1) currentIndex += 1
+    if (movedBy > 100 && currentIndex > 0) currentIndex -= 1
     setPositionByIndex()
+    container.classList.remove('grabbing')
 }
 
 function animation() {
@@ -100,7 +104,7 @@ function animation() {
 
 function setPositionByIndex() {
     prevTranslate = currenTranslate
-    setSliderPosition()
+    setSliderPosition();
 }
 
 function setSliderPosition() {
@@ -147,7 +151,7 @@ next.addEventListener("click", () => {
         if (!goingRight){
             currentImg--;
         }
-        if (currentImg >= img.length -1  && goingRight){
+        if (currentImg >= img.length - 1  && goingRight){
             scrollDirection = false;
         } else if (currentImg <= 0 && !goingRight){
             scrollDirection = true;
@@ -240,18 +244,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const videos = vidContainer.querySelectorAll('video');
     let currentVid = 0;
     let interval;
-    let scrollDirection = true;
+    let scrollDirection2 = true;
 
     next.addEventListener("click", () => {
         clearInterval(interval);
-        if (currentVid >= videos.length - 1) {
-            scrollDirection = false;
+        if (currentVid >= videos.length - 4) {
+            scrollDirection2 = false;
         } else {
             updateVid(true);
-            scrollDirection = true;
+            scrollDirection2 = true;
         }
         interval = setInterval(() => {
-            updateVid(scrollDirection);
+            updateVid(scrollDirection2);
         }, 3000);
     });
 
@@ -260,34 +264,39 @@ document.addEventListener('DOMContentLoaded', () => {
         clearInterval(interval);
         if (currentVid <= videos.length) {
             updateVid(false);
-            scrollDirection = false;
+            scrollDirection2 = false;
         } else {
-            scrollDirection = false;
+            scrollDirection2 = false;
         }
         interval = setInterval(() => {
-            updateVid(scrollDirection);
+            updateVid(scrollDirection2);
         }, 3000)
     });
 
 
     interval = setInterval(() => {
-        updateVid(scrollDirection);
-    }, 2000)
+        updateVid(scrollDirection2);
+    }, 3000)
 
     function updateVid(vidRight) {
+        console.log(vidRight);
+        console.log("--------------");
+        console.log(currentVid)
         if (vidRight) {
             currentVid++;
         }  if (!vidRight) {
             currentVid--;
         }
-        if (currentVid >= videos.length  && vidRight) {
-            scrollDirection = false;
-        } else if (currentVid <= 0 && !vidRight) {
-            scrollDirection = true;
-        }
-        vidContainer.style.transform = `translateX(-${(currentVid) * 37}em)`;
-    }
+        if (currentVid >= videos.length - 4  && vidRight) {
+            scrollDirection2 = false;
+            updateVid(false);
+        } if (currentVid <= (videos.length === 0) && !vidRight) {
+            scrollDirection2 = true;
+        } else if (currentVid >= 10){
 
+        }
+        vidContainer.style.transform = `translateX(-${(currentVid) * 50}%)`;
+    }
 })
 
 
